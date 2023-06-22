34c3b478a5c0f236585a74e731cd6330cfb41f28
Harshal Kamdi <118383371+harshal1714@users.noreply.github.com>
2023-06-22 17:29:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+const btn = document.getElementsByTagName("input")[0];
+
+const btnHandler = () => {
+  
+const val = document.getElementById("colorSelect").value;
+  const option = document.getElementById("colorSelect").children; 
+  for(let i=0; i<option.length; i++){
+    if(option[i].innerText === val){
+      option[i].remove();
+      return;
+    }
+  }
+}
+
+btn.addEventListener("click", btnHandler)
